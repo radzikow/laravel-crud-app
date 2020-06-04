35b129e65a35095030b5e16c9b04deb7dada052c
Radoslaw Bura <radoslaw.bura@gmail.com>
2020-06-04 10:24:52 +0200

    Updated ProductController, changed DB::table('products') to Product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 use App\Product;
 
